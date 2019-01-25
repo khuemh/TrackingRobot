55f0f5244065958dcfe66189721e70517a16ebdb
khuemh <khuehm@outlook.com>
2019-01-25 10:47:21 +0700

    hehe

--- ncstream2.sh
+++
@@ -1,1 +0,0 @@
-raspivid  -t 0 -w 640 -h 480 -hf -ih -fps 30 --rotation 180 -o - | nc -k -l 5557
--- vclsteam.sh
+++
@@ -1,1 +0,0 @@
-raspivid -o - -t 0 -n -w 640 -h 480 -fps 30 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264

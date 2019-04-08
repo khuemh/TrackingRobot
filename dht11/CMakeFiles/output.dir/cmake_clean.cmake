7b636e875887c77f87d1bf6702dfd5165ee918a8
khuehm <you@example.com>
2019-04-08 10:45:20 +0100

    send

---
+++ dht11/CMakeFiles/output.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/output.dir/dht11.c.o"
+  "output.pdb"
+  "output"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- dht11/send.cpp
+++ dht11/send.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <wiringPi.h>
 #include "Socket.h"
 
 using namespace std;
 
-int buff[2];
+//int buff[2] = {111, 222};
 
 int main(int argc, char const *argv[])
 {
-    buff[0] = 222;
-    buff[1] = 111;
-
-
     if ((argc < 3) || (argc > 3)) 
     { // Test for correct number of arguments
         cerr << "Usage: " << argv[0] << " <Server> <Server Port>\n";
@@ -19,11 +16,14 @@ int main(int argc, char const *argv[])
 
     string servAddress = argv[1]; // First arg: server address
     unsigned short servPort = Socket::resolveService(argv[2], "udp");
-
-    UDPSocket sock;
-
-    sock.sendTo(buff, 2, servAddress, servPort);
-
-    delay(1000);
+    
+        UDPSocket sock;
+        while(1)
+        {
+            int buff[2] = {111, 222};
+            sock.sendTo(buff, sizeof(buff), servAddress, servPort);
+            cout << "sent" << endl;
+            delay(1000);
+        }
     return 0;
 }
